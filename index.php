4622da71d7260e224ca438b0ef6131d181f6e129
Garvit Khatri <garvitdelhi@gmail.com>
2015-04-15 20:27:11 +0530

    fiz

--- index.php
+++ index.php
@@ -27,10 +27,6 @@ if(isset($_POST['id']) && isset($_POST['status'])) {
 	$mysqli->query($q);
 }
 
-/*
- * This is the "official" OO way to do it,
- * BUT $connect_error was broken until PHP 5.2.9 and 5.3.0.
- */
 if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
